efc301437222556fe58f72433d5536c260f9d2d7
Maria-crivel <mariacrivellente1234567890@gmail.com>
2022-06-30 20:38:05 -0400

    Enter

--- projetopython.py
+++ projetopython.py
@@ -184,8 +184,15 @@ print(type(pedido[1]))
 print(type(pedido[2]))
 print(type(pedido[3]))
 '''
-
+'''
 # 1. Declare as Tuplas acima
 t1 = ('Arroz', 'Feijão', 'Batata')
 t2 = (0, 6, 4, 2, 8, 6, 4)
 t3 = (1, 3, 3, 5, 3, 5, 7)
+'''
+# 2. Calcule o tamanho das tuplas: t1 e t2
+tamanhoT1 = len(t1)
+tamanhoT2 = len(t2)
+
+print(tamanhoT1)
+print(tamanhoT2)
