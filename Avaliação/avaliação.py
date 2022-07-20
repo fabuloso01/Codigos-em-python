e1e5bbe3deb0583105f8c6a12be70f0ac0bc7886
Maria-crivel <mariacrivellente1234567890@gmail.com>
2022-07-19 20:11:18 -0400

    First commit

---
+++ Avaliação/avaliação.py
@@ -0,0 +1,24 @@
+
+'''
+# math
+import math
+print(math.factorial(5))
+print(math.sqrt(49))
+
+from math import sqrt
+print(sqrt(49))
+
+from math import sqrt as raiz
+print(raiz(49))
+*/
+
+'''
+
+# Area de um circulo
+from math import pi
+
+raio = 4
+
+area = pi * (raio * raio)
+
+print(area)
