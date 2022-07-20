669b658743d9224c2737a2d3ff0be19ec798f333
Maria-crivel <mariacrivellente1234567890@gmail.com>
2022-07-19 20:21:35 -0400

    commit

--- projetopython.py
+++ projetopython.py
@@ -422,4 +422,48 @@ pessoas = {"nome"  : "Fulano",
 for chave, valor in pessoas.items():
   print(chave, valor)
 '''
+'''
+# math
+import math
+print(math.factorial(5))
+print(math.sqrt(49))
+
+from math import sqrt
+print(sqrt(49))
+
+from math import sqrt as raiz
+print(raiz(49))
+*/
+
+'''
+'''
+# Area de um circulo
+from math import pi
+
+raio = 4
+
+area = pi * (raio * raio)
+
+print(area)
+'''
+'''
+# datetime
+import datetime
+
+datetime.date.today()
+print(datetime.date.today())
+print(datetime.date.today().day)
+print(datetime.date.today().month)
+print(datetime.date.today().year)
+'''
+'''
+# from datetime
+from datetime import date
+dia = date.today().day
+mes = date.today().month
+ano = date.today().year
+print(f"{dia}/{mes}/{ano}")
+'''
+
+
 
